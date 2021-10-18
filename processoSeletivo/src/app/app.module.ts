c2116924880a77ce6a24f6cdeabef5ce38f9a302
Kevin Kurt <kekevekevin@gmail.com>
2021-10-18 00:44:16 -0300

    colocando rotas

--- processoSeletivo/src/app/app.module.ts
+++ processoSeletivo/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +12,9 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
     CadastrarComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    RouterTestingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
